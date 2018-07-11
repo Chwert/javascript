b96a1d909e398c2e89dc1d698703f524d588afc2
Oleksandr Moroz <ostapenkomoroz@gmail.com>
2018-07-11 16:22:16 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+    document.getElementById('paragraf').innerHTML = 'Привіт, JavaScript'; /* Вставляє в елемент з ідент. paragraf
+                                                                                    текст після innerHTML */
+}
